6dd1f381cdf1877760f00842cddea8dcd24fe4fc
Olcay Alkan <89521437+Teknowm3@users.noreply.github.com>
2025-01-16 05:04:45 +0300

    Thats all
    needed to add firebase

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: ['@pinia/nuxt'],
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css',
+    'bootstrap-icons/font/bootstrap-icons.css',
+  ],
+})
